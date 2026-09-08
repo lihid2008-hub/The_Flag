4f7c5677bd9c87d23399e882a8e50722478dfc72
Morashiga <noa.liberman3@gmail.com>
2026-09-08 16:41:37 +0300

    function is win is lose and locations of solider

--- Soldier.py
+++ Soldier.py
@@ -1,13 +1,23 @@
 import consts
 
 def get_soldier_parts(field):
-    body, legs = [], []
+    body= []
     count = consts.SOLDIER_BODY_ROWS * consts.SOLDIER_COLS
     for row in range(consts.BOARD_ROWS):
         for col in range(consts.BOARD_COLS):
             if count == len(body):
-                get_soldier_legs(body)
+                return body
             elif field[row][col] == "player":
                 body.append([row, col])
-    return body, legs
+    return body
 
+def get_soldier_legs(field):
+    legs = []
+    count = consts.SOLDIER_FEET_ROWS * consts.SOLDIER_COLS
+    for row in reversed(range(consts.BOARD_ROWS)):
+        for col in reversed(range(consts.BOARD_COLS)):
+            if count == len(legs):
+                return legs
+            elif field[row][col] == "player":
+                legs.append([row, col])
+    return legs
